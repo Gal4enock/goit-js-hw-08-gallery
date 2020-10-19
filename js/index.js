b62b0cbb095229f7eb2dd1f3e39f4bb82dbbedf6
Galyna Khyzhynska <gal4enock86@gmail.com>
2020-10-19 22:34:20 +0300

    добавляет решение

---
+++ js/index.js
@@ -0,0 +1,24 @@
+import gallery from './gallery-items.js';
+const list = document.querySelector(".js-gallery");
+const lightBox = document.querySelector('.lightbox');
+const btn = document.querySelector('.lightbox__button');
+const img = document.querySelector('.lightbox__image');
+const overlay = document.querySelector('.lightbox__overlay');
+console.log(list);
+for (let picture of gallery) {
+  const { preview, original, description } = picture;
+  list.insertAdjacentHTML('beforeend', `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></li>`);
+}
+const getPicture = function (e) {
+  e.preventDefault();
+  lightBox.classList.add('is-open');
+  const link = e.target.dataset.source;
+  img.setAttribute('src', `${link}`)
+}
+const closeModal = function () {
+  lightBox.classList.remove('is-open');
+  img.removeAttribute('src')
+}
+list.addEventListener('click', getPicture);
+btn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
