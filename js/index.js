7b5c21b010df801a7b3dce86259302dfa1072b67
Galyna Khyzhynska <gal4enock86@gmail.com>
2020-10-20 16:25:27 +0300

    добавляет исправления

--- js/index.js
+++ js/index.js
@@ -4,32 +4,28 @@ const lightBox = document.querySelector('.lightbox');
 const btn = document.querySelector('.lightbox__button');
 const img = document.querySelector('.lightbox__image');
 const overlay = document.querySelector('.lightbox__overlay');
-console.log(list);
-
-// for (let picture of gallery) {
-//   const { preview, original, description } = picture;
-//   list.insertAdjacentHTML('beforeend', `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></li>`);
-// }
 
 gallery.map(element => {
   const { preview, original, description } = element;
   list.insertAdjacentHTML('beforeend', `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></li>`);
 })
 
+const toggleAtr = function (attr = "") {
+  lightBox.classList.toggle('is-open');
+  attr ? img.setAttribute('src', `${attr}`) : img.removeAttribute('src');
+}
+
 const getPicture = function (e) {
+  if (e.target.nodeName !== "IMG") return;
   e.preventDefault();
-  lightBox.classList.add('is-open');
   const link = e.target.dataset.source;
-  img.setAttribute('src', `${link}`);
-  btn.addEventListener('click', closeModal);
-  overlay.addEventListener('click', closeModal);
+  toggleAtr(link);
+  lightBox.addEventListener('click', closeModal);
 }
 
 const closeModal = function () {
-  lightBox.classList.remove('is-open');
-  img.removeAttribute('src');
-  btn.removeEventListener('click', closeModal);
-  overlay.removeEventListener('click', closeModal);
+  toggleAtr();
+  lightBox.removeEventListener('click', closeModal);
 }
 
 list.addEventListener('click', getPicture);
